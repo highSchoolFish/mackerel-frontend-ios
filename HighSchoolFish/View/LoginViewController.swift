525a47dc6219537a24eb964ad3df8b24a60de229
bhyn98 <bhyn9785@gmail.com>
2023-11-14 14:55:33 +0900

    test commit

--- HighSchoolFish/View/LoginViewController.swift
+++ HighSchoolFish/View/LoginViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SwiftUI
 
 final class LoginViewController: UIViewController {
-    
+    var test = ""
     var fCurTextfieldBottom: CGFloat = 0.0
     private let viewModel = LoginViewModel()
     
