cb55210640688bad1efc1165c933e9e2f4a25065
bhyn98 <bhyn9785@gmail.com>
2023-11-15 19:20:36 +0900

    feat: 게시판 상세조회 및 댓글 조회 구현

---
+++ HighSchoolFish/View/CommentHeaderReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  CommentHeaderReusableView.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 11/14/23.
+//
+
+import UIKit
+
+class CommentHeaderReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
