45e66dcfce6af10ccf3a16df667edc74d5ce7442
bhyn98 <bhyn9785@gmail.com>
2024-01-10 19:02:03 +0900

    feat: NoticeViewController UI 구현

---
+++ HighSchoolFish/View/NoticeTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeTableViewCell.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 1/8/24.
+//
+
+import Foundation
---
+++ HighSchoolFish/ViewModel/NoticeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeViewModel.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 1/10/24.
+//
+
+import Foundation
