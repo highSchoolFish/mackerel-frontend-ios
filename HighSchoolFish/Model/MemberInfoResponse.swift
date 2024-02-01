b9ddba8775e9c24b75efb507ac599c3a8a0dcf90
bhyn98 <bhyn9785@gmail.com>
2024-02-01 18:07:37 +0900

    feat: MemberInfo service 추가 및 게시판 등록 service 변경

---
+++ HighSchoolFish/Model/MemberInfoResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MemberInfoResponse.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 2/1/24.
+//
+
+import Foundation
---
+++ HighSchoolFish/ViewModel/MemberInfoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MemberInfoViewModel.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 2/1/24.
+//
+
+import Foundation
