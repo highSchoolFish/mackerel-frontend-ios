79dd55adce2ad9f3d87bb62b93a9c5dce616487d
bhyn98 <bhyn9785@gmail.com>
2024-02-07 18:54:23 +0900

    feat: BottomSheetVC 버튼 동작 및 CustomAlert 구현

---
+++ HighSchoolFish/Model/CustomAlertData.swift
@@ -0,0 +1,8 @@
+//
+//  CustomAlertData.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 2/7/24.
+//
+
+import Foundation
---
+++ HighSchoolFish/ViewModel/CustomAlertViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CustomAlertViewModel.swift
+//  HighSchoolFish
+//
+//  Created by 강보현 on 2/7/24.
+//
+
+import Foundation
